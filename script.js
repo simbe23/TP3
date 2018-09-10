3b97edb2c64b8bdde2cbd40fdeb02586979d51eb
simbe23 <simbe23@users.noreply.github.com>
2018-09-10 11:10:21 -0300

    Bootstrap a todo

--- script.js
+++ script.js
@@ -43,25 +43,7 @@ function setBindings() {
 		});
 
 
-
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the header
 var header = $("#mynav")[0];
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
 
 
 $("#submit").click(function(){
